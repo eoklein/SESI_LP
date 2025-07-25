9aa13e618d516ecd46b7fba60651a1fbfda3e470
eoklein <otimosprimevascodagama@gmail.com>
2025-07-25 10:38:49 -0300

    C

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+float main()
+{
+    int vlr1;
+    int vlr2;
+    
+    printf("Digite o valor do primeiro numero: ");
+    scanf("%d", &vlr1);
+    
+    printf("Digite o valor do segundo numero: ");
+    scanf("%d", &vlr2);
+     
+    int soma = vlr1 + vlr2;
+    int multi = vlr1 * vlr2;
+    int divisao = vlr1 / vlr2;
+    
+    printf("%d + %d = %d\n", vlr1, vlr2, soma);
+    printf("%d x %d = %d\n", vlr1, vlr2, multi);
+    printf("%d / %d = %d\n", vlr1, vlr2, divisao);
+
+}
